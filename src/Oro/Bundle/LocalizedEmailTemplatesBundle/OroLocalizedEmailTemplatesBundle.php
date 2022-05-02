09927d7166118624a5d11a555c277adc8d4f2687
Vova Soroka <vsoroka@oroinc.com>
2022-05-02 22:23:13 +0300

    BAP-21348: Remove unneeded getAlias() and getContainerExtension() methods (#32522)
    - remove unneeded getAlias() method in bundle Extension classes
    - remove unneeded getContainerExtension() method in Bundle classes
    - remove check for comments for Bundle classes

--- src/Oro/Bundle/LocalizedEmailTemplatesBundle/OroLocalizedEmailTemplatesBundle.php
+++ src/Oro/Bundle/LocalizedEmailTemplatesBundle/OroLocalizedEmailTemplatesBundle.php
@@ -4,9 +4,6 @@ namespace Oro\Bundle\LocalizedEmailTemplatesBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * The LocalizedEmailTemplate bundle class
- */
 class OroLocalizedEmailTemplatesBundle extends Bundle
 {
 }
